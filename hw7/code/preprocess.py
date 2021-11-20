c90909fb0ed0bccfbb333e82ca1251b18f686704
Brendan Ho <brendan_ho@brown.edu>
2021-11-20 18:44:10 -0500

    made a large amount of progress

--- hw7/code/preprocess.py
+++ hw7/code/preprocess.py
@@ -1,9 +1,25 @@
 import gzip
 import numpy as np
+import json as js
 
-def get_data(inputs_file_path, labels_file_path, num_examples):
-	"""
-	Takes in an inputs file path and labels file path
-    """
-	
-	pass
+# Builds a dictionary with three fields id pair and same, which each contain a list where each list index is a docpair
+def get_data(inputs_file_path, labels_file_path):
+    di = {}
+    di['id'] = []
+    di['same'] = []
+    di['pair'] = []
+    with open(inputs_file_path) as f, open(labels_file_path) as g:
+        for line in f:
+            data = js.loads(line)
+            di['id'].append(data['id'])
+            di['pair'].append(data['pair'])
+
+        for line in g:
+            data = js.loads(line)
+            di['same'].append(data['same'])
+
+    print(di['id'][0])
+    print(di['same'][0])
+    print(di['pair'][0])
+  
+get_data("data/pan20-authorship-verification-training-small.jsonl", "data/pan20-authorship-verification-training-small-truth.jsonl")    
