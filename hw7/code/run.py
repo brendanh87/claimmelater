d08cbdeecc0d2fc186919e72bbbd13fc8aea7293
Brendan Ho <brendan_ho@brown.edu>
2021-11-30 20:31:47 -0500

    preprocessing

--- hw7/code/run.py
+++ hw7/code/run.py
@@ -3,29 +3,16 @@ import os
 import random
 import tensorflow as tf
 from preprocess import get_data
-from preprocess import read_count_vectors
+from preprocess import read_data
 from model import Model
-
-def train(model, train_inputs, train_labels):
-    # stochastic gradient descent
-    for batch in range(model.batch_size):
-        # store starting and ending indices for batch
-        start = batch_number * model.batch_size
-        end = start + model.batch_size
-        # gradient descend that bad boi
-        model.call(train_inputs[start : end], train_labels[start : end])
-
-
-def test(model, test_inputs, test_labels):
-    pass
 
 def main():
     # intialize the model
     model = Model()
-    # get the train_inputs, train_labels
-    train_inputs, train_labels = get_data('data/pan20-authorship-verification-training-small.jsonl', 'data/pan20-authorship-verification-training-small-truth.jsonl')
-    
-    # stochastic gradient descent
+    # get the inputs and labels for training and testing
+    train_inputs, test_inputs, train_labels, test_labels = read_data('hw7/code/count-vectors.npy', 'hw7/code/labels.npy')
+
+    # train the model with stochastic gradient descent
     for batch in range(model.batch_size):
         # store starting and ending indices for batch
         start = batch_number * model.batch_size
@@ -33,5 +20,4 @@ def main():
         # gradient descend that bad boi
         model.call(train_inputs[start : end], train_labels[start : end])
 
-    
-    test(model, test_inputs, test_labels)
+    # test the accuracy of the model
