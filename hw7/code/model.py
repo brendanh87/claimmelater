959eace5e71424a99f1d64341dcc67c0f953d042
Alyssa Loo <Alyssa_MARIE_from.TP@tech.gov.sg>
2021-11-30 19:45:02 -0500

    more changes

--- hw7/code/model.py
+++ hw7/code/model.py
@@ -7,6 +7,7 @@ from tensorflow.keras import Model
 from tensorflow.keras import layers
 from tensorflow.keras import Lambda
 from preprocess import get_data
+from tensorflow.keras import metrics
 
 
 class Model(tf.keras.Model):
@@ -23,6 +24,7 @@ class Model(tf.keras.Model):
         self.res_layer_count = 8
         self.optimizer = tf.keras.optimizers.Adam(self.learning_rate)
         self.embedding_size = (1, 11675)
+        self.batch_size = 120
         # self.siamese_hidden_dim = 512
         # self.classifier_hidden_dim = 256
 
@@ -45,8 +47,9 @@ class Model(tf.keras.Model):
         siameseA_output = self.subnetwork(siameseA_input)
         siameseB_input = keras.Input(self.embedding_size)
         siameseB_output = self.subnetwork(siameseB_input)
+        distance = Lambda(lambda x, y: tf.linalg.norm(x - y, axis = 1))([siameseA_output, siameseB_output])
         self.siamese = keras.Model(inputs = [siameseA_input, siameseB_input], 
-                                   outputs = [siameseA_output, siameseB_output],
+                                   outputs = distance,
                                    name = 'siamese')
         # classifier model
         siameseA_output = keras.Input(shape = (1, 512))
@@ -71,13 +74,12 @@ class Model(tf.keras.Model):
         return x   
 
     def call(self, inputs, labels):
-        self.siamese.compile(loss = self.loss, optimizer = self.optimizer)
-        self.siamese.fit([inputs[0], inputs[1]], labels, batch_size = 120)
+        self.siamese.compile(loss = metrics.contrastive_loss, optimizer = self.optimizer)
+        self.siamese.fit([inputs[0], inputs[1]], labels, batch_size = self.batch_size)
 
 
     def loss(self, feature1, feature2, labels, margin=1.0):
         # get the difference of the two feature vectors
-        predictions = tf.linalg.norm(feature1 - feature2, axis=1)
         # get the contrastive loss
         loss = tf.math.reduce_mean(tfa.losses.contrastive_loss(labels, predictions, margin=margin))
         return loss
