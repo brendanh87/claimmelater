3fe8268d35309123a354888b910d175c905c71f2
Alyssa Loo <Alyssa_MARIE_from.TP@tech.gov.sg>
2021-12-09 15:06:51 -0500

    fixed demo

--- hw7/code/demo.py
+++ hw7/code/demo.py
@@ -1,11 +1,13 @@
 from preprocess import vectorize
 from model import Model, load_classifier_model
 import string
+import numpy as np
+import tensorflow as tf
 
 DICT_FILE_PATH = "output_dictionary.pkl"
-PATH_TO_WEIGHTS = "../../weights/split_model_weights/split_model_weights"
+PATH_TO_WEIGHTS = "../../weights/100_whole_addition/whole_model_weights"
 PATH_TO_FILE_A = "robert.txt"
-PATH_TO_FILE_B = "deathlyhallows.txt"
+PATH_TO_FILE_B = "papertowns.txt"
 
 def lower_n_unpunc(text):
     return text.translate(str.maketrans('','', string.punctuation)).replace('\n', ' ').replace("  "," ").lower()
@@ -17,11 +19,10 @@ def predict_for_texts(path_to_file_a, path_to_file_b):
     with open(path_to_file_b, 'r') as file_b:
        text_b = lower_n_unpunc(file_b.read())
 
-    vectors = vectorize(DICT_FILE_PATH, [text_a, text_b])
-           
+    vectors = np.array(vectorize(DICT_FILE_PATH, [text_a, text_b]))
     model = Model()
     load_classifier_model(model, PATH_TO_WEIGHTS)
-    print(model.call(vectors))
+    print(model.call(tf.expand_dims(vectors[0], 0), tf.expand_dims(vectors[1], 0)))
 
 predict_for_texts(PATH_TO_FILE_A, PATH_TO_FILE_B)
 
